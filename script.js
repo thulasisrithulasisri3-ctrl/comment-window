a929b61e5026f58002e1902d8ebc8590b36da464
R. Thulasi sri <thulasisrithulasisri3@gmail.com>
2026-08-11 22:39:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,5 +14,24 @@ function addComment() {
     comment.textContent = commentText;
 
     commentList.appendChild(comment);
+
+    let comments = JSON.parse(localStorage.getItem("comments")) || [];
+    comments.push(commentText);
+    localStorage.setItem("comments", JSON.stringify(comments));
+
     input.value = "";
 }
+
+function loadComments() {
+    const commentList = document.getElementById("commentList");
+    const comments = JSON.parse(localStorage.getItem("comments")) || [];
+
+    comments.forEach(function(text) {
+        const comment = document.createElement("div");
+        comment.className = "comment";
+        comment.textContent = text;
+        commentList.appendChild(comment);
+    });
+}
+
+window.onload = loadComments;
